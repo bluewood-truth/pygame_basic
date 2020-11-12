9d3d902aac4f541ac3d649c43135a4e042fa3ca1
bluewood-truth <55024033+bluewood-truth@users.noreply.github.com>
2020-11-12 16:34:23 +0900

    캐릭터 이동 구현
    화면 내에서 캐릭터 이동을 구현함.

--- basic/4_keyboard_event.py
+++ basic/4_keyboard_event.py
@@ -19,12 +19,43 @@ character_width, character_height = character.get_rect().size
 character_x_pos = (screen_width - character_width) / 2
 character_y_pos = screen_height - character_height
 
+# 이동할 좌표
+to_x = to_y = 0
+
 # 이벤트 루프
 running = True
 while running: # Unity의 Update()와 같은 역할
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트 발생시 (x버튼, Ctrl+F4 등)
             running = False # 이벤트 루프를 끝낸다
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:
+                to_x += 5
+            elif event.key == pygame.K_UP:
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:
+                to_y += 5
+        
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                to_x = 0
+            elif event.key in (pygame.K_UP, pygame.K_DOWN):
+                to_y = 0
+
+    # 캐릭터 이동 및 화면 경계 처리
+    character_x_pos += to_x
+    character_y_pos += to_y
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
 
     screen.blit(background, (0,0)) # 배경 그리기
     screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
